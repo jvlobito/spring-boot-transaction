e068f01e46cfeb9d9cd89fd00b5c3a1418183e99
jvicgalicia <jvgali@yahoo.com.mx>
2025-02-08 23:40:57 -0600

    Cambios para empaquetar war

--- src/main/java/mx/examvic/com/RestApiApplication.java
+++ src/main/java/mx/examvic/com/RestApiApplication.java
@@ -2,13 +2,19 @@ package mx.examvic.com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class RestApiApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestApiApplication.class, args);
 	}
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestApiApplication.class);
+	}
 }
